11f094aaccc9438c7e26a818e0f6fe8c06daba56
Evgeniy <01evgeniyshlyk@gmail.com>
2023-07-16 13:44:50 +0300

    calculator

--- calc.js
+++ calc.js
@@ -1,2 +1,12 @@
-let a = 'hello world'
-alert (a);
+function calc (operation, a, b){
+    if(operation === 'add'){
+        return a + b;
+    }else if(operation === 'multi'){
+        return a * b;
+    }else if (operation === 'subs'){
+        return a - b
+    }
+}
+console.log(calc('add', 5, 3)) //8
+console.log(calc('multi', 5, 5)) //25
+console.log(calc('subs', 10, 2)) //8
