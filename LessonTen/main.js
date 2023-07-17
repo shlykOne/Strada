3eaf5a9fcbf159c856e02f3011e9acf8ffe4205d
Evgeniy <01evgeniyshlyk@gmail.com>
2023-07-17 18:14:22 +0300

    LessonTen

---
+++ LessonTen/main.js
@@ -0,0 +1,106 @@
+/*
+let i = 1;
+while( i < 6){
+    console.log (i)
+    i++
+}
+
+let i = 1;
+while(i < 3) console.log(i++);
+
+
+let i = 1;
+do{
+    console.log(i)
+    i++
+}while(i<1)
+
+let i = 1;
+while(i < 20){
+    console.log(i)
+    i++
+
+let i = 1 
+do{
+    console.log(i)
+    i++
+}while(i < 20);}
+
+for (i = 1; i < 4; i++){
+    console.log(i)
+}
+
+
+for (i = 1; i < 4; i++){
+    console.log(i)  
+}
+let i = 1
+for(; i < 4; i++){
+    console.log(i)
+
+for (i = 1; i < 20; i++){
+    console.log(i)
+}
+while(true){
+    console.log('Диалог 1');
+    console.log('Диалог 2');
+    break
+    console.log('Диалог 3');
+}}
+let i = 1;
+while(i < 5){
+    console.log(i)
+    i++
+}
+let i = 1;
+while(i < 5){
+    console.log(i);
+    i++
+}
+
+let i = 1;
+while(i < 4 ){
+    console.log(i)
+    i++
+
+let i = 10
+do{
+    console.log(i)
+    i++
+}while(i < 10);
+
+let i = 3;
+while(i){
+    console.log(i)
+    i--
+}
+
+let i = 3;
+while(i < 10) console.log(i++)
+let i = 0 
+do{
+    console.log(i) // Сначало выполнит тело, т.е это 0
+    i++
+}while(i < 0);
+
+for(let i = 1; i < 4; i++){
+    console.log(i)
+}
+
+let a = 2;
+for(; a < 5; a++){
+    console.log(a)
+}
+
+while (true) {
+    console.log('Текст 1');
+    console.log('Текст 2');
+    break
+    console.log('текст 3 ');
+}
+console.log('текст 4')
+*/
+for (i = 1; i < 10; i++){
+    if(i%2 == 0) continue
+    console.log(i)
+}
