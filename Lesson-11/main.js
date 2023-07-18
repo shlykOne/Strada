f68199369b3c8b7800f8b6c0222dbab2abfe1472
Evgeniy <01evgeniyshlyk@gmail.com>
2023-07-18 16:04:31 +0300

    Lesson-11

---
+++ Lesson-11/main.js
@@ -0,0 +1,28 @@
+/*
+const info = {
+    'Evgeniy': 2552,
+    'igor': 1845,
+    'Artem': 27809,
+    'John Smith': 255668
+}
+//console.log(info); - возвращает в консоль все ключи с их знач
+//console.log(info.Evgeniy) // 2552
+//console.log(info["John Smith"]) // 255668
+info['John Smith'] = 1234 // Изменение значение ключа
+info["Ilya"] = 2552 // Добавление нового ключа со знач
+
+delete info ['John Smith']; // Удаление записи John Smith
+console.log(info)
+
+const info = {
+    list:{
+        'Evgeniy': 2552,
+        'Ilya': 255668,
+        'Anton': 12443,
+        'Artem': 255120
+    },
+    log(){
+        console.log(this.list)
+    }
+}
+info.log()*/
